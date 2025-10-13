1dd2c1ce768792bb6c885309728ecd1b675834c7
thuynt1 <thuynt1@vivas.vn>
2025-10-13 09:26:33 +0700

    add user_services

--- lib/screens/user/user_management_screen.dart
+++ lib/screens/user/user_management_screen.dart
@@ -15,38 +15,40 @@ class _UserManagementScreenState extends State<UserManagementScreen> {
   Widget build(BuildContext context) {
     return BlocConsumer<UserManagementBloc, UserManagementState>(
       builder: (context, newState) {
-        return Row(
+        return Column(
           children: [
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 100),
-                child: Row(
-                  children: [
-                    InkWell(
-                      onTap: () async {},
-                      child: Container(
-                        color: Colors.blue,
-                        child: Text('Thêm Người Dùng'),
-                      ),
+            Padding(
+              padding: const EdgeInsets.only(left: 100),
+              child: Row(
+                children: [
+                  InkWell(
+                    onTap: () async {},
+                    child: Container(
+                      color: Colors.blue,
+                      child: Text('Thêm Người Dùng', ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
             SizedBox(height: 200),
-            ListView.separated(
-              itemCount: 100,
-              separatorBuilder: (_, __) => const Divider(height: 1),
-              itemBuilder: (context, i) {
-                final name = "Thuy";
-                return ListTile(
-                  title: Text(name),
-                  leading: CircleAvatar(
-                    child: Text(name.isNotEmpty ? name[0].toUpperCase() : '?'),
-                  ),
-                  onTap: () {},
-                );
-              },
+            Flexible(
+              child: ListView.separated(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: 100,
+                separatorBuilder: (_, __) => const Divider(height: 1),
+                itemBuilder: (context, i) {
+                  final name = "Thuy";
+                  return ListTile(
+                    title: Text(name),
+                    leading: CircleAvatar(
+                      child: Text(name.isNotEmpty ? name[0].toUpperCase() : '?',style: TextStyle(color:Colors.red )),
+                    ),
+                    onTap: () {},
+                  );
+                },
+              ),
             ),
           ],
         );
