1b03e3240e1ce1ead1e57a697d20c2fea76f1963
Ha Thi Phuong <phuonght@vivas.vn>
2025-10-13 13:05:09 +0700

    remove duplicate

--- lib/domain/entities/camera/camera_status.dart
+++ lib/domain/entities/camera/camera_status.dart
@@ -43,32 +43,3 @@ extension CameraStatusExt on CameraStatus {
     }
   }
 }
-
-extension CameraStatusExt on CameraStatus {
-  String getName() {
-    switch (this) {
-      case CameraStatus.active:
-        return 'Online';
-      default:
-        return 'Offline';
-    }
-  }
-
-  Color get contentColor {
-    switch (this) {
-      case CameraStatus.active:
-        return AppColors.primary;
-      default:
-        return AppColors.grey6F767E;
-    }
-  }
-
-  Color get bgColor {
-    switch (this) {
-      case CameraStatus.active:
-        return AppColors.blueD7F1FF;
-      default:
-        return AppColors.scaffoldBgLight;
-    }
-  }
-}
