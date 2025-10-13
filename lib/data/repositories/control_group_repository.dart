a31c7960fd138d21d9288f723945eae1d1b96996
hieunt1 <hieunt1@vivas.vn>
2025-10-13 10:17:45 +0700

    refactor: streamline camera validation and response handling in ControlCameraService and repositories

--- lib/data/repositories/control_group_repository.dart
+++ lib/data/repositories/control_group_repository.dart
@@ -1,7 +1,6 @@
 import 'package:vms_flutter_client/core/base_response.dart';
 import 'package:vms_flutter_client/data/datasources/camera_service.dart';
 import 'package:vms_flutter_client/data/mappers/camera_mapper.dart';
-import 'package:vms_flutter_client/data/proto/models/comm.command1.pb.dart';
 import 'package:vms_flutter_client/domain/entities/camera/camera_entity.dart';
 import 'package:vms_flutter_client/domain/entities/camera/camera_onvif.dart';
 import 'package:vms_flutter_client/domain/i_repositories/i_control_camera_repository.dart';
@@ -14,7 +13,7 @@ class ControlCameraRepository extends BaseRepository implements IControlCameraRe
   const ControlCameraRepository(this.service);
 
   @override
-  Future<Either<Failure, CheckCameraOnvif?>> validateCamera({String? message}) {
+  Future<Either<Failure, CameraOnvif?>> validateCamera({String? message}) {
     throw UnimplementedError();
   }
 
--- lib/domain/i_repositories/i_control_camera_repository.dart
+++ lib/domain/i_repositories/i_control_camera_repository.dart
@@ -1,15 +1,10 @@
 import 'package:vms_flutter_client/core/base_response.dart';
-import 'package:vms_flutter_client/data/proto/models/comm.command1.pb.dart';
 import 'package:vms_flutter_client/domain/entities/camera/camera_entity.dart';
 import 'package:vms_flutter_client/domain/entities/camera/camera_onvif.dart';
 
 abstract class IControlCameraRepository {
-  Future<Either<Failure, CheckCameraOnvif?>> validateCamera({String? message});
-  Future<Either<Failure, List<CameraEntity>>> getAllCamera({
-    List<int>? cameraId,
-    int? status,
-    int? ivaType,
-  });
+  Future<Either<Failure, CameraOnvif?>> validateCamera({String? message});
+  Future<Either<Failure, List<CameraEntity>>> getAllCamera({List<int>? cameraId, int? status, int? ivaType});
   Future<Either<Failure, CameraOnvif>> checkCameraOnvif({
     required String xaddrs,
     required String userName,
