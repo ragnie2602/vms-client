a4d64517210d356f6c9cf4f0e719265ab12199f1
hieunt1 <hieunt1@vivas.vn>
2025-10-15 16:53:40 +0700

    refactor: integrate BlocListener for camera state updates and streamline UI structure in control camera screen

--- lib/screens/control_camera/control_camera_screen.dart
+++ lib/screens/control_camera/control_camera_screen.dart
@@ -128,161 +128,158 @@ class _ControlCameraScreenState extends State<ControlCameraScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: Colors.white),
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-            margin: EdgeInsets.only(bottom: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Flexible(
-                  flex: 2,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Tên nhóm camera', style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400)),
-                      const SizedBox(height: 10),
-                      TextField(
-                        controller: cameraNameController,
-                        decoration: InputDecoration(
-                          prefixIcon: Container(
-                            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
-                            child: SvgPicture.asset(AppAssets.icSearch),
+    return BlocListener<ControlCameraBloc, ControlCameraState>(
+      listenWhen: (prev, curr) => curr is DeleteCameraSuccessState || curr is ListCameraSuccessState,
+      listener: (context, state) {
+        if (state is DeleteCameraSuccessState) {
+          _onGetListCamera();
+          showAppMessageDialog(context, message: 'Xóa camera thành công!', type: AppMessageType.success);
+        } else if (state is ListCameraSuccessState) {
+          setState(() {});
+        }
+      },
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: Colors.white),
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              margin: EdgeInsets.only(bottom: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Flexible(
+                    flex: 2,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Tên nhóm camera', style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400)),
+                        const SizedBox(height: 10),
+                        TextField(
+                          controller: cameraNameController,
+                          decoration: InputDecoration(
+                            prefixIcon: Container(
+                              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+                              child: SvgPicture.asset(AppAssets.icSearch),
+                            ),
+                            hintText: 'Nhập tên camera',
+                            hintStyle: TextStyle(color: AppColors.grey64748B, fontSize: 14, fontWeight: FontWeight.w400),
+                            enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: AppColors.greyE2E8F0, width: 1)),
+                            focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: AppColors.greyE2E8F0, width: 1)),
+                            border: UnderlineInputBorder(),
                           ),
-                          hintText: 'Nhập tên camera',
-                          hintStyle: TextStyle(color: AppColors.grey64748B, fontSize: 14, fontWeight: FontWeight.w400),
-                          enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: AppColors.greyE2E8F0, width: 1)),
-                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: AppColors.greyE2E8F0, width: 1)),
-                          border: UnderlineInputBorder(),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(width: 15),
-                Flexible(
-                  flex: 1,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Trạng thái', style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400)),
-                      const SizedBox(height: 10),
-                      CustomCommonDropdown<CameraStatus>(
-                        items: CameraStatus.values,
-                        value: cameraStatus,
-                        onChanged: (p0) {
-                          setState(() {
-                            cameraStatus = p0;
-                          });
+                  const SizedBox(width: 15),
+                  Flexible(
+                    flex: 1,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Trạng thái', style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400)),
+                        const SizedBox(height: 10),
+                        CustomCommonDropdown<CameraStatus>(
+                          items: CameraStatus.values,
+                          value: cameraStatus,
+                          onChanged: (p0) {
+                            setState(() {
+                              cameraStatus = p0;
+                            });
+                          },
+                          itemAsString: (p0) => p0.getName(),
+                          contentTextStyle: AppTypography.style(14, fontWeight: FontWeight.w400, color: AppColors.grey64748B),
+                          hint: Text(
+                            'Tất cả',
+                            style: AppTypography.style(14, fontWeight: FontWeight.w400, color: AppColors.grey64748B),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      _onSearch();
+                    },
+                    splashColor: Colors.transparent,
+                    child: Container(
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(3), color: AppColors.secondary),
+                      padding: EdgeInsets.symmetric(vertical: 12, horizontal: 18),
+                      margin: EdgeInsets.only(left: 15),
+                      child: Center(child: SvgPicture.asset(AppAssets.icSearch, color: Colors.white)),
+                    ),
+                  ),
+                  const SizedBox(width: 25),
+                  InkWell(
+                    onTap: () {
+                      showAddCameraRtspDialog(
+                        context,
+                        onSubmit: (payload) async {
+                          if (payload.method == 'RTSP') {
+                            _onAddCameraRTSP(
+                              name: payload.name,
+                              username: payload.username,
+                              password: payload.password,
+                              rtspUrl: payload.rtsp,
+                              location: payload.location,
+                              boxId: const [],
+                              groupId: const [],
+                              subStreamUrls: payload.subStreamUrls,
+                            );
+                          } else {
+                            _onAddCameraOnvif(
+                              name: payload.name,
+                              username: payload.username,
+                              password: payload.password,
+                              onvifDeviceIp: payload.onifDeviceIp,
+                              rtspUrl: payload.rtsp,
+                              serialNumber: '',
+                              location: payload.location,
+                              boxId: const [],
+                              groupId: const [],
+                              urn: '',
+                              subStreamUrls: payload.subStreamUrls,
+                            );
+                          }
                         },
-                        itemAsString: (p0) => p0.getName(),
-                        contentTextStyle: AppTypography.style(14, fontWeight: FontWeight.w400, color: AppColors.grey64748B),
-                        hint: Text(
-                          'Tất cả',
-                          style: AppTypography.style(14, fontWeight: FontWeight.w400, color: AppColors.grey64748B),
+                        onCheck: (xaddrs, userName, password, boxId) {
+                          _onCheckOnvif(xaddrs: xaddrs, userName: userName, password: password, boxId: boxId);
+                        },
+                      );
+                    },
+                    splashColor: Colors.transparent,
+
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(3),
+                        border: Border.all(width: 1, color: AppColors.secondary),
+                      ),
+                      child: Center(
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            SvgPicture.asset(AppAssets.icAdd),
+                            const SizedBox(width: 8),
+                            Text(
+                              'Thêm camera',
+                              style: TextStyle(color: AppColors.secondary, fontSize: 14, fontWeight: FontWeight.w500),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
-                ),
-                InkWell(
-                  onTap: () {
-                    _onSearch();
-                  },
-                  splashColor: Colors.transparent,
-                  child: Container(
-                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(3), color: AppColors.secondary),
-                    padding: EdgeInsets.symmetric(vertical: 12, horizontal: 18),
-                    margin: EdgeInsets.only(left: 15),
-                    child: Center(child: SvgPicture.asset(AppAssets.icSearch, color: Colors.white)),
-                  ),
-                ),
-                const SizedBox(width: 25),
-                InkWell(
-                  onTap: () {
-                    showAddCameraRtspDialog(
-                      context,
-                      onSubmit: (payload) async {
-                        if (payload.method == 'RTSP') {
-                          _onAddCameraRTSP(
-                            name: payload.name,
-                            username: payload.username,
-                            password: payload.password,
-                            rtspUrl: payload.rtsp,
-                            location: payload.location,
-                            boxId: const [],
-                            groupId: const [],
-                            subStreamUrls: payload.subStreamUrls,
-                          );
-                        } else {
-                          _onAddCameraOnvif(
-                            name: payload.name,
-                            username: payload.username,
-                            password: payload.password,
-                            onvifDeviceIp: payload.onifDeviceIp,
-                            rtspUrl: payload.rtsp,
-                            serialNumber: '',
-                            location: payload.location,
-                            boxId: const [],
-                            groupId: const [],
-                            urn: '',
-                            subStreamUrls: payload.subStreamUrls,
-                          );
-                        }
-                      },
-                      onCheck: (xaddrs, userName, password, boxId) {
-                        _onCheckOnvif(xaddrs: xaddrs, userName: userName, password: password, boxId: boxId);
-                      },
-                    );
-                  },
-                  splashColor: Colors.transparent,
-
-                  child: Container(
-                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(3),
-                      border: Border.all(width: 1, color: AppColors.secondary),
-                    ),
-                    child: Center(
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          SvgPicture.asset(AppAssets.icAdd),
-                          const SizedBox(width: 8),
-                          Text(
-                            'Thêm camera',
-                            style: TextStyle(color: AppColors.secondary, fontSize: 14, fontWeight: FontWeight.w500),
-                          ),
-                        ],
-                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          BlocListener<ControlCameraBloc, ControlCameraState>(
-            listener: (context, state) {
-              if (state is DeleteCameraSuccessState) {
-                showAppMessageDialog(
-                  context,
-                  message: 'Xóa camera thành công!',
-                  type: AppMessageType.success,
-                  onOk: () {
-                    _onGetListCamera();
-                  },
-                );
-              }
-            },
-            child: Container(
+            Container(
               decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: Colors.white),
               padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
               child: Column(
@@ -331,8 +328,8 @@ class _ControlCameraScreenState extends State<ControlCameraScreen> {
                 ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
