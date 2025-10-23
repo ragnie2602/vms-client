c09b0f00f25f1848610181e1780fd21130119c75
Ha Thi Phuong <phuonght@vivas.vn>
2025-10-23 09:41:40 +0700

    fix: update add group

--- lib/screens/group/group_camera_view.dart
+++ lib/screens/group/group_camera_view.dart
@@ -292,6 +292,7 @@ class _GroupCameraViewState extends State<GroupCameraView> {
                                         listGroupInput: newState.groups ?? [],
                                         c: context,
                                         addEditType: AddEditGroupType.add,
+                                        parentGroupId: node.data?.groupId,
                                       );
                                       break;
                                     case ItemGroupAction.edit:
