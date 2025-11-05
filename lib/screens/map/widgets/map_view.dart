596a00d930d76f670b1fdfa7057c5763f6b4ae23
thuynt1 <thuynt1@vivas.vn>
2025-11-05 11:31:20 +0700

    add add camera

--- lib/screens/map/widgets/map_view.dart
+++ lib/screens/map/widgets/map_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:vms_flutter_client/core/constants/assets.dart';
 import 'package:vms_flutter_client/core/constants/colors.dart';
 import 'package:vms_flutter_client/core/constants/typography.dart';
 
@@ -9,15 +11,124 @@ class MapView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-      child: Center(
-        child: Text(
-          'Chưa có bản đồ camera nào. Click + để thêm bản đồ camera',
-          style: AppTypography.style(
-            14,
-            fontWeight: FontWeight.w400,
-            color: AppColors.grey64748B,
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.white,
+            ),
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            margin: EdgeInsets.all(10),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.3,
+                      height: 40,
+                      child: TextField(
+                        onChanged: (value) {},
+
+                        decoration: InputDecoration(
+                          prefixIcon: Container(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 12,
+                              horizontal: 12,
+                            ),
+                            child: SvgPicture.asset(AppAssets.icSearch),
+                          ),
+                          hintText: 'Nhập tên camera',
+                          hintStyle: AppTypography.style(
+                            14,
+                            color: AppColors.grey64748B,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 8,
+                            horizontal: 12,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(4),
+                            borderSide: BorderSide(
+                              color: AppColors.greyE2E8F0,
+                              width: 1,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(4),
+                            borderSide: BorderSide(
+                              color: AppColors.greyE2E8F0,
+                              width: 1,
+                            ),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(4),
+                            borderSide: BorderSide(
+                              color: AppColors.greyE2E8F0,
+                              width: 1,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    InkWell(
+                      onTap: () async {},
+                      splashColor: Colors.transparent,
+
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 8,
+                          horizontal: 12,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(3),
+                          border: Border.all(
+                            width: 1,
+                            color: AppColors.secondary,
+                          ),
+                        ),
+                        child: Center(
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              SvgPicture.asset(AppAssets.icAdd),
+                              const SizedBox(width: 8),
+                              Text(
+                                'Thêm camera',
+                                style: AppTypography.style(
+                                  14,
+                                  color: AppColors.secondary,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
+          Expanded(
+            child: Center(
+              child: Text(
+                'Chưa có bản đồ camera nào. Click + để thêm bản đồ camera',
+                style: AppTypography.style(
+                  14,
+                  fontWeight: FontWeight.w400,
+                  color: AppColors.grey64748B,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
