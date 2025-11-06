03335a6fb4dd118a17b64cebc51e3858333c13aa
Ha Thi Phuong <phuonght@vivas.vn>
2025-11-06 15:37:08 +0700

    feat: update validate

--- lib/screens/map/widgets/dash_border_widget.dart
+++ lib/screens/map/widgets/dash_border_widget.dart
@@ -1,14 +1,18 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:vms_flutter_client/core/constants/colors.dart';
 
 class DashedBorderPainter extends CustomPainter {
+  final bool? isError;
+  DashedBorderPainter({this.isError});
+
   @override
   void paint(Canvas canvas, Size size) {
     const double dashWidth = 6;
     const double dashSpace = 4;
     final Paint paint = Paint()
-      ..color = Colors.grey.shade300
+      ..color = isError == true ? AppColors.redFF0004 : Colors.grey.shade300
       ..strokeWidth = 1
       ..style = PaintingStyle.stroke;
 
@@ -24,8 +28,10 @@ class DashedBorderPainter extends CustomPainter {
       double distance = 0.0;
       while (distance < metric.length) {
         final double end = distance + dashWidth;
-        final Path extractPath =
-            metric.extractPath(distance, end.clamp(0, metric.length));
+        final Path extractPath = metric.extractPath(
+          distance,
+          end.clamp(0, metric.length),
+        );
         canvas.drawPath(extractPath, paint);
         distance += dashWidth + dashSpace;
       }
