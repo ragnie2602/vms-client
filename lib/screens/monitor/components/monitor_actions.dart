27e75bd31e1d711c1f14f92a0ccf00e53a71c9e1
DESKTOP-VEN79LN\admin <tuend@vivas.vn>
2025-11-18 13:45:44 +0700

    feat(camera): update logic for opening camera detail from camera panel

--- lib/screens/monitor/components/monitor_actions.dart
+++ lib/screens/monitor/components/monitor_actions.dart
@@ -95,9 +95,14 @@ class _MonitorActionsState extends State<MonitorActions> {
                         maxWidth: widget.leftController.expandedWidth,
                         key: ValueKey('monitor_cameras'),
                         onTap: (data) {
-                          context.pushNamed(
+                          context.goNamed(
                             Routes.cameraDetail.name,
-                            extra: CameraDetailScreenArgs(data: data),
+                            extra: CameraDetailScreenArgs(
+                              data: data,
+                              onBack: () => AppRouter.rootNavigatorKey.currentContext?.goNamed(
+                                Routes.monitoring.name,
+                              ),
+                            ),
                           );
                         },
                       ),
