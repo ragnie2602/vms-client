6262ce314792bf7919d815683a15eb5f43a30ddc
Ragnie <p.lyquangk.lythedan@gmail.com>
2025-11-20 09:15:45 +0700

    refactor: do no depend on data layer

--- lib/domain/entities/tag/tag_entity.dart
+++ lib/domain/entities/tag/tag_entity.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:vms_flutter_client/data/proto/models/comm.model.pb.dart';
 
 class TagEntity {
   final String id;
@@ -7,11 +6,6 @@ class TagEntity {
   final Color color;
 
   TagEntity({required this.id, required this.name, required this.color});
-
-  TagEntity.fromResponse(CamTag tag)
-    : id = tag.tagId.toString(),
-      name = tag.tagName,
-      color = Color(int.parse(tag.tagColor));
 
   TagEntity copyWith({String? id, String? name, Color? color}) {
     return TagEntity(id: id ?? this.id, name: name ?? this.name, color: color ?? this.color);
