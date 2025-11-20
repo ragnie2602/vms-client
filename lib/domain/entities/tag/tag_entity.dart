310b8bc1f5df584dbac1691c06e1a86f34a55a2e
Ragnie <p.lyquangk.lythedan@gmail.com>
2025-11-20 10:27:25 +0700

    hot fix

--- lib/domain/entities/tag/tag_entity.dart
+++ lib/domain/entities/tag/tag_entity.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class TagEntity {
-  final String id;
+  final List<int> id;
   final String name;
   final Color color;
 
   TagEntity({required this.id, required this.name, required this.color});
 
-  TagEntity copyWith({String? id, String? name, Color? color}) {
+  TagEntity copyWith({List<int>? id, String? name, Color? color}) {
     return TagEntity(id: id ?? this.id, name: name ?? this.name, color: color ?? this.color);
   }
 }
